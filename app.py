26c1b43058f91f689602eba208f672e3b6884a0e
Jay Mark Ugay <57929491+jayzien09@users.noreply.github.com>
2023-03-24 20:35:04 +0000

    link job description html that is connected to the db

--- app.py
+++ app.py
@@ -4,20 +4,25 @@ from database import load_jobs_from_db, load_job_from_db
 app = Flask(__name__)
 
 
-@app.route("/") #so example facebook/jaymark ung jaymark ung route
+@app.route("/")  #so example facebook/jaymark ung jaymark ung route
 def intro_header():
   jobs = load_jobs_from_db()
   return render_template('home.html', jobs=jobs)
+
 
 @app.route("/api/jobs")
 def list_jobs():
   jobs = load_jobs_from_db()
   return jsonify(jobs)
 
+
 @app.route("/job/<id>")
 def show_job(id):
-  job_description = load_job_from_db(id)
-  return jsonify(job_description)
+  job = load_job_from_db(id)
+  if not job:
+    return "Not Found", 404
+  return render_template('jobpage.html', job=job)
+
 
 if __name__ == '__main__':
   app.run(host='0.0.0.0', debug=True)
--- database.py
+++ database.py
@@ -1,32 +1,27 @@
-from sqlalchemy import create_engine,text
-import os
-
+from sqlalchemy import create_engine, text
 
 db_connection = "mysql+pymysql://sity5u6oymhxpjs7360w:pscale_pw_yhPz4gS8cWglL6KjUgSYiYVPXWMDgSedi57EcJ7Qtzi@ap-southeast.connect.psdb.cloud/trial_jaymark?charset=utf8mb4"
 
+engine = create_engine(db_connection,
+                       connect_args={"ssl": {
+                         "ssl_cert": "/etc/ssl/cert.pem"
+                       }})
 
-engine = create_engine(
-    db_connection,
-    connect_args={
-        "ssl": {
-            "ssl_cert": "/etc/ssl/cert.pem"
-        }
-    }
-)
 
 def load_jobs_from_db():
   with engine.connect() as conn:
     result_jobs = conn.execute(text("select * from jobs"))
     all_jobs = result_jobs.all()
-    column_names = result_jobs.keys() 
-    
+    column_names = result_jobs.keys()
+
     job_dict = []
-    
+
     for row in all_jobs:
       job_dict.append(dict(zip(column_names, row)))
-  
+
     return job_dict
-    
+
+
 """def load_job_from_db(id):
   with engine.connect() as conn:
     result_job = conn.execute(
@@ -40,18 +35,23 @@ def load_jobs_from_db():
       row_dict = dict(zip(row_key[0], row[0]))
       return row_dict"""
 
+
 def load_job_from_db(id):
-    with engine.connect() as conn:
-        result_job = conn.execute(
-            text("SELECT * FROM jobs WHERE id = :id").bindparams(id=id)
-        )
-        row = result_job.all()
-        row_key = result_job.keys()
+  with engine.connect() as conn:
+    result_job = conn.execute(
+      text("SELECT * FROM jobs WHERE id = :id").bindparams(id=id))
+    rows = result_job.all()
+    row_key = result_job.keys()
 
-        if len(row) == 0:
-            return None
-        else:
-            print(dict(zip(row_key, row)))
-            return dict(zip(row_key, row))
-          
+    if len(rows) == 0:
+      return None
+    else:
+      job_dicts = []
+      for row in rows:
+        job_dict = dict(zip(row_key, row))
+        job_dicts.append(job_dict)
+      return job_dicts[0]
 
+
+#load_jobs_from_db()
+#oad_job_from_db(1)
